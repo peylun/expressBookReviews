f595426e0d0dbad4a43e23034b6835a9b4124af3
Hsieh Pey Lun <peylun@gmail.com>
2025-07-18 11:03:52 -0400

    Feat: User login
    Feat: Add book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -11,20 +11,52 @@ const isValid = (username) => {
     return !foundUser;
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+    const foundUser = users.find((user) => user.username === username && user.password === password);
+
+    return Boolean(foundUser);
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(400).json({ message: "Error logging in" });
+    }
+
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            username,
+        }, 'access', { expiresIn: 60 * 60 });
+
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(401).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    if (!req.query.review) {
+        return res.status(400).json({ message: "Invalid request." });
+    }
+
+    const foundBook = books[req.params.isbn];
+    if (foundBook) {
+        foundBook.reviews[req.user.username] = req.query.review;
+        return res.status(200).send("Added book review.");
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
 });
 
 module.exports.authenticated = regd_users;
