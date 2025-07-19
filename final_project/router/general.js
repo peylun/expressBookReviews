5dd03849f8c4dc3f4efbaafaabb9b45cd79b055f
Hsieh Pey Lun <peylun@gmail.com>
2025-07-19 04:37:35 -0400

    Clean ups

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -44,61 +44,71 @@ public_users.post("/register", (req,res) => {
         if (isValid(username)) {
             // Add the new user to the users array
             users.push({"username": username, "password": password});
-            return res.status(200).json({message: "User successfully registered."});
+            return res.status(200).json({ message: "User successfully registered." });
         } else {
-            return res.status(400).json({message: "User already exists!"});
+            return res.status(400).json({ message: "User already exists!" });
         }
     } else {
         // Return error if username or password is missing
-        return res.status(404).json({message: "Unable to register user."});
+        return res.status(404).json({ message: "Unable to register user." });
     }
 });
 
 // Get the book list available in the shop
 public_users.get('/', async function (req, res) {
-    const fetchedBooks = await fetchBooks();
-    const bookList = Object.values(fetchedBooks);
-    return res.send(JSON.stringify(bookList, null, 4));
+    try {
+        const fetchedBooks = await fetchBooks();
+        return res.status(200).send(JSON.stringify(fetchedBooks, null, 4));
+    } catch (ex) {
+        return res.status(500).send({ message: "Internal server error." })
+    }
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', async function (req, res) {
-    const foundBook = await fetchBookByIsbn(req.params.isbn);
-    if (foundBook) {
-        return res.send(JSON.stringify(foundBook, null, 4))
-    } else {
-        return res.send(null);
+    try {
+        const foundBook = await fetchBookByIsbn(req.params.isbn);
+        if (foundBook) {
+            return res.status(200).send(JSON.stringify(foundBook, null, 4))
+        } else {
+            return res.status(404).send({ message: "Book not found." });
+        }
+    } catch (ex) {
+        return res.status(500).send({ message: "Internal server error." })
     }
 });
   
-// Get book details based on author
+// Get all books based on author
 public_users.get('/author/:author', async function (req, res) {
-    const foundBooks = await fetchBooksByAuthor(req.params.author);
-    if (foundBooks) {
-        return res.send(JSON.stringify(foundBooks, null, 4))
-    } else {
-        return res.send(null);
+    try {
+        const foundBooks = await fetchBooksByAuthor(req.params.author);
+        return res.status(200).send(JSON.stringify(foundBooks, null, 4))
+    } catch (ex) {
+        return res.status(500).send({ message: "Internal server error." })
     }
 });
 
 // Get all books based on title
 public_users.get('/title/:title', async function (req, res) {
-    const foundBooks = await fetchBooksByTitle(req.params.title);
-    if (foundBooks) {
-        return res.send(JSON.stringify(foundBooks, null, 4))
-    } else {
-        return res.send(null);
+    try {
+        const foundBooks = await fetchBooksByTitle(req.params.title);
+        return res.status(200).send(JSON.stringify(foundBooks, null, 4))
+    } catch (ex) {
+        return res.status(500).send({ message: "Internal server error." })
     }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-    const foundBook = books[req.params.isbn];
-    if (foundBook) {
-        // Should omit returning username; skipping here as this is mainly for illustration
-        return res.send(JSON.stringify(foundBook.reviews, null, 4))
-    } else {
-        return res.send(null);
+    try {
+        const foundBook = books[req.params.isbn];
+        if (foundBook) {
+            return res.status(200).send(JSON.stringify(foundBook.reviews, null, 4))
+        } else {
+            return res.status(404).send({ message: "Book not found." });
+        }
+    } catch (ex) {
+        return res.status(500).send({ message: "Internal server error." })
     }
 });
 
